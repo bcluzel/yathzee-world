8f9f2ed9c2795f5d606097449a9e0d1bfb99430e
Baptiste Cluzel <baptiste.clu@gmail.com>
2025-09-09 09:59:34 +0200

    backend: add config and init actix session

---
+++ backend/src/config.rs
@@ -0,0 +1,38 @@
+use actix_web::cookie::Key;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    #[serde(with = "hex")]
+    pub key: Vec<u8>,
+}
+
+fn read_config(path: &String) -> Config {
+    let config_str = fs::read_to_string(path).unwrap();
+    toml::from_str(&config_str).unwrap()
+}
+
+fn save_config(path: &String, config: &Config) {
+    let config_str = toml::to_string_pretty(config).unwrap();
+    fs::write(path, config_str).unwrap();
+}
+
+pub fn load_config() -> Config {
+    let workdir_path = "./workdir";
+    if !fs::exists(workdir_path).unwrap() {
+        log::info!("Workdir not found, creating.");
+        fs::create_dir(workdir_path).unwrap();
+    }
+    let config_path = workdir_path.to_string() + "/config.toml";
+    log::info!("Config path: {}", config_path);
+    if fs::exists(&config_path).unwrap() {
+        read_config(&config_path)
+    } else {
+        let config = Config {
+            key: Key::generate().master().into(),
+        };
+        save_config(&config_path, &config);
+        config
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,25 +1,40 @@
+use actix_identity::IdentityMiddleware;
+use actix_session::{SessionMiddleware, storage::CookieSessionStore};
 use actix_web::dev::{ServiceRequest, ServiceResponse, fn_service};
-use actix_web::{App, HttpResponse, HttpServer};
+use actix_web::{App, HttpResponse, HttpServer, cookie::Key};
 use log;
 mod api;
+mod config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
     log::info!("yathee-world init");
-    HttpServer::new(|| {
-        App::new().configure(api::scoped_api).service(
-            actix_files::Files::new("/", "./static-frontend")
-                .prefer_utf8(true)
-                .index_file("index.html")
-                .default_handler(fn_service(|req: ServiceRequest| async {
-                    let (req, _) = req.into_parts();
-                    Ok(ServiceResponse::new(
-                        req,
-                        HttpResponse::NotFound().body("Build failed, contact website owner"),
-                    ))
-                })),
-        )
+    let config = config::load_config();
+    log::info!("Starting http server");
+
+    let session_secret_key = Key::from(config.key.as_slice());
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(IdentityMiddleware::default())
+            .wrap(SessionMiddleware::new(
+                CookieSessionStore::default(),
+                session_secret_key.clone(),
+            ))
+            .configure(api::scoped_api)
+            .service(
+                actix_files::Files::new("/", "./static-frontend")
+                    .prefer_utf8(true)
+                    .index_file("index.html")
+                    .default_handler(fn_service(|req: ServiceRequest| async {
+                        let (req, _) = req.into_parts();
+                        Ok(ServiceResponse::new(
+                            req,
+                            HttpResponse::NotFound().body("Build failed, contact website owner"),
+                        ))
+                    })),
+            )
     })
     .bind(("0.0.0.0", 8080))?
     .run()
